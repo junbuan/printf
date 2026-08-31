af56dcc71e08b36fc187e86dd1f4e64554089b2e
Jun Hong <junh161108@gmail.com>
2026-08-31 21:48:26 +0800

    .

---
+++ ft_printf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juho <juho@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/27 14:49:46 by juho              #+#    #+#             */
+/*   Updated: 2026/08/31 21:47:55 by juho             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include "libft/libft.h"
+
+# define HEX_UP "0123456789ABCDEF"
+# define HEX_LOW "0123456789abcdef"
+# define DECIMAL "0123456789"
+
+#endif
