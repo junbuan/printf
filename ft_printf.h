eeff94755f26863c6ed9f352f756b36be16e7fed
Jun Hong <junh161108@gmail.com>
2026-09-01 13:27:48 +0800

    .

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: juho <juho@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/27 14:49:46 by juho              #+#    #+#             */
-/*   Updated: 2026/08/31 21:47:55 by juho             ###   ########.fr       */
+/*   Updated: 2026/09/01 13:27:39 by juho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,9 @@
 # define HEX_UP "0123456789ABCDEF"
 # define HEX_LOW "0123456789abcdef"
 # define DECIMAL "0123456789"
+
+int	ft_putunbr_pf(unsigned long n);
+int	ft_putptr_pf(void *ptr);
+int	ft_puthex_pf(unsigned long n, char *base);
 
 #endif
