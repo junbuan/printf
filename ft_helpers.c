f101b7f81d56a37e6debea960bb88f8ab69ed19c
Jun Hong <junh161108@gmail.com>
2026-09-03 17:15:54 +0800

    .

--- ft_helpers.c
+++ ft_helpers.c
@@ -6,18 +6,37 @@
 /*   By: juho <juho@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/30 15:36:07 by juho              #+#    #+#             */
-/*   Updated: 2026/09/03 14:49:57 by juho             ###   ########.fr       */
+/*   Updated: 2026/09/03 17:07:14 by juho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_putstr_pf(char * s)
+int	ft_putstr_pf(char *s)
 {
-	write(1, s, ft_strlen(s));
+	int	len;
+
+	len = 0;
+	if (!s)
+		return (write(1, "(null)", 6), 6);
+	while (*s)
+		len += write(1, *s++, 1);
+	return (len);
 }
 
-	
+int	ft_putnbr_pf(long n)
+{
+	int	i;
+
+	i = 0;
+	if (n < 0)
+	{
+		i += write(1, "-", 1);
+		n = -n;
+	}
+	return (i + ft_putunbr_pf((unsigned long)n));
+}
+
 int	ft_putunbr_pf(unsigned long n)
 {
 	int	i;
@@ -33,7 +52,7 @@ int	ft_putptr_pf(void *ptr)
 {
 	if (!ptr)
 		return (ft_putstr_pf("(nil)"));
-	return (ft_putstr_pf("0x") + ft_puthex_pf((unsigned long) ptr, HEX_LOW));
+	return (ft_putstr_pf("0x") + ft_puthex_pf((unsigned long long) ptr, HEX_LOW));
 }
 
 int	ft_puthex_pf(unsigned long n, char *base)
